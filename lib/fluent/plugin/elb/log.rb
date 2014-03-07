827beff75cc274421320dbdc52e2332547ae1dda
shinsaka <shinx1265@gmail.com>
2014-03-07 15:02:15 +0900

    first commit

---
+++ lib/fluent/plugin/elb/log.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/elb/log/version"
+
+module Fluent
+  module Plugin
+    module Elb
+      module Log
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/elb/log/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Elb
+      module Log
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
