995dcc9642f1f00dcab9ebf78ccedbf1ce38f00a
shinsaka <shinx1265@gmail.com>
2014-03-09 13:48:13 +0900

    walked through implementation

--- README.md
+++ README.md
@@ -7,3 +7,65 @@
 
     $ fluentd-gem fluent-plugin-elb-log
 
+## AWS ELB Settings
+- settings see: [Elastic Load Balancing](http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/enable-access-logs.html)
+- developer guide: [](http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/access-log-collection.html)
+
+## Configuration
+
+```config
+<source>
+  type elb_log
+  access_key_id     <access_key>
+  secret_access_key <secret_access_key>
+  s3_endpoint       <s3_endpoint>
+  s3_bucketname     <bucketname>
+  s3_prefix         <elb log's prefix>
+  timestamp_file    <proc last file timestamp record filename>
+  refresh_interval  <interval number by second>
+</source>
+```
+
+### Example setting
+```config
+<source>
+  type elb_log
+  access_key_id     XXXXXXXXXXXXXXXXXXXX
+  secret_access_key xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  s3_endpoint       s3.amazonaws.com
+  s3_bucketname     bucketname
+  s3_prefix         prefix
+  timestamp_file    elb_last_at.dat
+  refresh_interval  300
+</source>
+
+<match **>
+  type stdout
+</match>
+```
+{
+    "account_id": "999999999999", 
+    "backend": "192.168.30.127", 
+    "backend_port": "80", 
+    "backend_processing_time": "0.000985", 
+    "backend_status_code": "200", 
+    "client": "118.20.x.x", 
+    "client_port": "46171", 
+    "elb": "fluent-test-elb", 
+    "elb_ip_address": "54.250.x.x", 
+    "elb_status_code": "200", 
+    "logfile_date": "2014/03/09", 
+    "logfile_elb_name": "fluent-test-elb", 
+    "logfile_hash": "xyz123ab", 
+    "received_bytes": "0", 
+    "region": "ap-northeast-1", 
+    "request_method": "GET", 
+    "request_processing_time": "0.000072", 
+    "request_protocol": "HTTP/1.1", 
+    "request_uri": "http://logfile_elb_name-00000000.ap-northeast-1.elb.amazonaws.com:80/", 
+    "response_processing_time": "0.00007", 
+    "sent_bytes": "9", 
+    "time": "2014-03-09T04:10:33.785083Z"
+}
+```
+
--- lib/fluent/plugin/in_elb_log.rb
+++ lib/fluent/plugin/in_elb_log.rb
@@ -1,11 +1,16 @@
 class Fluent::Elb_LogInput < Fluent::Input
   Fluent::Plugin.register_input('elb_log', self)
+  
+  LOGFILE_REGEXP = /^((?<prefix>.+?)\/|)AWSLogs\/(?<account_id>[0-9]{12})\/elasticloadbalancing\/(?<region>.+?)\/(?<logfile_date>[0-9]{4}\/[0-9]{2}\/[0-9]{2})\/[0-9]{12}_elasticloadbalancing_.+?_(?<logfile_elb_name>[^_]+)_(?<elb_timestamp>[0-9]{8}T[0-9]{4}Z)_(?<elb_ip_address>.+?)_(?<logfile_hash>.+)\.log$/
+  ACCESSLOG_REGEXP = /^(?<time>.+?) (?<elb>.+?) (?<client>.+)\:(?<client_port>.+) (?<backend>.+)\:(?<backend_port>.+) (?<request_processing_time>.+?) (?<backend_processing_time>.+?) (?<response_processing_time>.+?) (?<elb_status_code>.+?) (?<backend_status_code>.+?) (?<received_bytes>.+?) (?<sent_bytes>.+?) \"(?<request_method>.+?) (?<request_uri>.+?) (?<request_protocol>.+?)\"$/
 
   config_param :access_key_id, :string, :default => nil
   config_param :secret_access_key, :string, :default => nil
   config_param :s3_bucketname, :string, :default => nil
+  config_param :s3_prefix, :string, :default => nil
   config_param :s3_endpoint, :string, :default => 's3.amazon.com'
   config_param :timestamp_file, :string, :default => nil
+  config_param :refresh_interval, :integer, :default => 300
 
   def configure(conf)
     super
@@ -19,14 +24,14 @@ class Fluent::Elb_LogInput < Fluent::Input
   def start
     super
 
-    init_s3bucket()
+    init_s3bucket
 
     @timestamp_file = File.open(@timestamp_file, File::RDWR|File::CREAT)
     @timestamp_file.sync = true
 
     @loop = Coolio::Loop.new
-    timer_trigger = TimerWatcher.new(60, true, &method(:on_notify))
-    timer_trigger 
+    timer_trigger = TimerWatcher.new(@refresh_interval, true, &method(:input))
+    timer_trigger.attach(@loop)
     @thread = Thread.new(&method(:run))
   end
 
@@ -40,17 +45,82 @@ class Fluent::Elb_LogInput < Fluent::Input
   private
 
   def init_s3bucket
-    @bucket = AWS::S3.new(
-      access_key_id: @aws_access_key_id,
+   @bucket = AWS::S3.new(
+      access_key_id: @access_key_id,
       secret_access_key: @secret_access_key
-    ).buckets[@aws_s3_bucketname]
+    ).buckets[@s3_bucketname]
   end
 
   def run
     @loop.run
-  rescue
-    log.error "unexpected error", :error=>$!.to_s
-    log.error_backtrace
+  end
+
+  def input
+    $log.info "fluent-plugin-elb-log: input start"
+   
+    #get timestamp last proc
+    @timestamp_file.rewind
+    timestamp = @timestamp_file.read.to_i
+    timestamp = 0 unless timestamp
+    $log.info "fluent-plugin-elb-log: timestamp at start: " + Time.at(timestamp).to_s
+
+    log_objects = []
+    @bucket.objects.each do |obj|
+      next if obj.last_modified.to_i <= timestamp
+      matches = LOGFILE_REGEXP.match(obj.key)
+      next unless matches
+      next if !@s3_prefix.nil? && matches[:prefix] != @s3_prefix
+      log_objects.push obj
+    end
+
+    # sort by timestamp
+    log_objects.sort! do |a,b|
+      LOGFILE_REGEXP.match(a.key)[:elb_timestamp] <=> LOGFILE_REGEXP.match(b.key)[:elb_timestamp]
+    end
+
+    log_objects.each do |obj|
+      matches = LOGFILE_REGEXP.match(obj.key)
+      timestamp = matches[:elb_timestamp].to_i
+      record_common = {
+        account_id: matches[:account_id],
+        region: matches[:region],
+        logfile_date: matches[:logfile_date],
+        logfile_elb_name: matches[:logfile_elb_name],
+        elb_ip_address: matches[:elb_ip_address],
+        logfile_hash: matches[:logfile_hash],
+      }
+
+      obj.read do |line|
+        line_match = ACCESSLOG_REGEXP.match(line)
+        next unless line_match
+
+        record = {
+          time: line_match[:time],
+          elb: line_match[:elb],
+          client: line_match[:client],
+          client_port: line_match[:client_port],
+          backend: line_match[:backend],
+          backend_port: line_match[:backend_port],
+          request_processing_time: line_match[:request_processing_time],
+          backend_processing_time: line_match[:backend_processing_time],
+          response_processing_time: line_match[:response_processing_time],
+          elb_status_code: line_match[:elb_status_code],
+          backend_status_code: line_match[:backend_status_code],
+          received_bytes: line_match[:received_bytes],
+          sent_bytes: line_match[:sent_bytes],
+          request_method: line_match[:request_method],
+          request_uri: line_match[:request_uri],
+          request_protocol: line_match[:request_protocol],
+        }
+        
+        Fluent::Engine.emit("elb.access", Fluent::Engine.now, record_common.merge(record))
+      end
+      # timestamp save
+      @timestamp_file.rewind
+      @timestamp_file.write(obj.last_modified.to_i)
+      @timestamp_file.truncate(@timestamp_file.tell)
+      $log.info "fluent-plugin-elb-log: timestamp save: " + obj.last_modified.to_s
+    end
   end
 
   class TimerWatcher < Coolio::TimerWatcher
@@ -61,9 +131,6 @@ class Fluent::Elb_LogInput < Fluent::Input
 
     def on_timer
       @callback.call
-    rescue
-      @log.error $!.to_s
-      @log.error_backtrace
     end
   end
 end
